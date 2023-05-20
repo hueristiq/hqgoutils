cbcf4d19ce84e5454227a72632b01bef6657edcb
Alex Munene (@enenumxela) <62714471+enenumxela@users.noreply.github.com>
2023-05-20 16:00:18 +0300

    refactor(url): -

--- url/url.go
+++ url/url.go
@@ -9,7 +9,7 @@ import (
 	"golang.org/x/net/publicsuffix"
 )
 
-// A URL represents a parsed URL (technically, a URI reference).
+// URL represents a parsed URL (technically, a URI reference).
 //
 // The general form represented is:
 //
@@ -45,40 +45,31 @@ type URL struct {
 // Parse parses a raw url into a URL structure.
 //
 // It uses the  `net/url`'s Parse() internally, but it slightly changes its behavior:
-//  1. It forces the default scheme if the url doesnt have a scheme and port to http
+//  1. It forces the default scheme, if the url doesnt have a scheme, to http
 //  2. It favors absolute paths over relative ones, thus "example.com"
 //     is parsed into url.Host instead of url.Path.
 //  3. It lowercases the Host (not only the Scheme).
 func Parse(rawURL string) (parsedURL *URL, err error) {
-	var (
-		defaultScheme string = "http"
-	)
+	const defaultScheme string = "http"
 
-	rawURL = DefaultScheme(rawURL, defaultScheme)
+	rawURL = AddDefaultScheme(rawURL, defaultScheme)
+
 	parsedURL = &URL{}
 
 	parsedURL.URL, err = url.Parse(rawURL)
 	if err != nil {
-		err = fmt.Errorf("[hqgoutils/url] %s", err)
+		err = fmt.Errorf("[hqgoutils/url]: %w", err)
 
 		return
 	}
 
 	// Host = Domain + Port
-	for i := len(parsedURL.URL.Host) - 1; i >= 0; i-- {
-		if parsedURL.URL.Host[i] == ':' {
-			parsedURL.Domain = parsedURL.URL.Host[:i]
-			parsedURL.Port = parsedURL.URL.Host[i+1:]
-			break
-		} else if parsedURL.URL.Host[i] < '0' || parsedURL.URL.Host[i] > '9' {
-			parsedURL.Domain = parsedURL.URL.Host
-		}
-	}
+	parsedURL.Domain, parsedURL.Port = SplitHost(parsedURL.URL.Host)
 
 	// ETLDPlusOne
 	parsedURL.ETLDPlusOne, err = publicsuffix.EffectiveTLDPlusOne(parsedURL.Domain)
 	if err != nil {
-		err = fmt.Errorf("[hqgoutils/url] %s", err)
+		err = fmt.Errorf("[hqgoutils/url] %w", err)
 
 		return
 	}
@@ -99,25 +90,28 @@ func Parse(rawURL string) (parsedURL *URL, err error) {
 	return
 }
 
-// DefaultScheme forces default scheme to `http` scheme, so net/url.Parse() doesn't
-// put both host and path into the (relative) path.
-func DefaultScheme(URL, scheme string) (URLWithScheme string) {
-	URLWithScheme = URL
-
-	// e.g //example.com
-	if strings.Index(URLWithScheme, "//") == 0 {
-		URLWithScheme = scheme + ":" + URLWithScheme
+// AddDefaultScheme ensures a scheme is added if none exists.
+func AddDefaultScheme(rawURL, scheme string) string {
+	switch {
+	case strings.HasPrefix(rawURL, "//"):
+		return scheme + ":" + rawURL
+	case strings.Contains(rawURL, "://") && !strings.HasPrefix(rawURL, "http"):
+		return scheme + rawURL
+	case !strings.Contains(rawURL, "://"):
+		return scheme + "://" + rawURL
+	default:
+		return rawURL
 	}
+}
 
-	// e.g ://example.com
-	if strings.Contains(URLWithScheme, "://") && !strings.HasPrefix(URLWithScheme, "http") {
-		URLWithScheme = scheme + URLWithScheme
+// splitHost splits the host into domain and port.
+func SplitHost(host string) (domain string, port string) {
+	for i := len(host) - 1; i >= 0; i-- {
+		if host[i] == ':' {
+			return host[:i], host[i+1:]
+		} else if host[i] < '0' || host[i] > '9' {
+			domain = host
+		}
 	}
-
-	// e.g example.com, localhost
-	if !strings.Contains(URLWithScheme, "://") {
-		URLWithScheme = scheme + "://" + URLWithScheme
-	}
-
 	return
 }
