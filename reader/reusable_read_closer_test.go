470895c62dbc97b6e2cd2dcc8b5db3865b1b65ab
Alex Munene (@enenumxela) <62714471+enenumxela@users.noreply.github.com>
2023-10-24 03:46:33 +0300

    feat(reader): Add ReusableReadCloser

---
+++ reader/reusable_read_closer_test.go
@@ -0,0 +1,70 @@
+package reader_test
+
+import (
+	"bytes"
+	"io"
+	"strings"
+	"sync"
+	"testing"
+
+	"github.com/hueristiq/hqgoutils/reader"
+	"github.com/stretchr/testify/require"
+)
+
+func TestReusableReader(t *testing.T) {
+	t.Parallel()
+
+	testcases := []interface{}{
+		strings.NewReader("test"),
+		bytes.NewBuffer([]byte("test")),
+		bytes.NewBufferString("test"),
+		bytes.NewReader([]byte("test")),
+		[]byte("test"),
+		"test",
+	}
+
+	for _, v := range testcases {
+		v := v
+
+		t.Run("sequential reuse", func(t *testing.T) {
+			t.Parallel()
+
+			reusableReader, err := reader.NewReusableReadCloser(v)
+			require.Nil(t, err)
+
+			for i := 0; i < 100; i++ {
+				n, err := io.Copy(io.Discard, reusableReader)
+				require.Nil(t, err)
+				require.Positive(t, n)
+
+				bin, err := io.ReadAll(reusableReader)
+				require.Nil(t, err)
+				require.Len(t, bin, 4)
+			}
+		})
+
+		// todo: readers shouldn't be used concurrently, so here we just try to catch pontential read concurring with resets panics
+		t.Run("concurrent-reset-with-read", func(t *testing.T) {
+			t.Parallel()
+
+			reusableReader, err := reader.NewReusableReadCloser(v)
+			require.Nil(t, err)
+
+			var wg sync.WaitGroup
+
+			for i := 0; i < 100; i++ {
+				wg.Add(1)
+				go func() {
+					defer wg.Done()
+
+					_, err := io.Copy(io.Discard, reusableReader)
+					require.Nil(t, err)
+					_, err = io.ReadAll(reusableReader)
+					require.Nil(t, err)
+				}()
+			}
+
+			wg.Wait()
+		})
+	}
+}
